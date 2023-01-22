4eb199a4253586ca0f93be44d5a1649ab0960b06
Jeff Smith <JeffSmithDeveloper@outlook.com>
2023-01-22 12:03:32 -0800

    Initial Commit

---
+++ MyOrginizer/MyOrginizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOrginizerApp.swift
+//  MyOrginizer
+//
+//  Created by Xenomorph on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MyOrginizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
